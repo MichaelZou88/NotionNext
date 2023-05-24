9dfb35d4118267b9a16fe62a50a84bd68397c788
MichaelZou88 <129649201+MichaelZou88@users.noreply.github.com>
2023-05-24 20:58:05 +0800

    Update Header.js
    400 to 100

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -24,7 +24,7 @@ const Header = props => {
       changeType(
         new Typed('#typed', {
           strings: CONFIG_HEXO.HOME_BANNER_GREETINGS,
-          typeSpeed: 400,
+          typeSpeed: 100,
           backSpeed: 100,
           backDelay: 400,
           showCursor: true,
