01cf74053b014ba9149375b7f8782d730f738542
MichaelZou88 <129649201+MichaelZou88@users.noreply.github.com>
2023-05-24 15:33:25 +0800

    Update config_hexo.js
    修改首页标语文字

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Hi, we are Maxwell New Energy', 'We pride ourselves as a leading manufacturer of solar products', 'Our expertise lies in solar panels, solar inverters, solar batteries, and solar street lights', 'Welcome you to Maxwell New Energy!🎉'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
